01aee2d5c1bf1c9fe3244cce01ea6fc203d08548
AnabelGRios <anabelgrios@gmail.com>
2016-02-08 20:27:41 +0100

    appBrujulaVoz comentada

--- appFotoVoz/java/Brujula.java
+++ appFotoVoz/java/Brujula.java
@@ -13,9 +13,28 @@ import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by jacin on 03/02/2016.
+/*  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    See <http://www.gnu.org/licenses/> for a copy of the GNU General
+    Public License.
+
+    Autores: Jacinto Carrasco Castillo, Anabel Gómez Ríos.
+    Fecha de la última modificación: 10/02/2016.
  */
+
+/* Esta clase es la responsable de orientar al usuario: mostrar la brújula y animarla, indicando en un mensaje
+   cómo tiene que orientarse (que será lo escuchado en la clase BrujulaVoz.java) e indicando con un
+   mensaje en pantalla que ya lo ha hecho cuando lo consiga.
+
+ */
+
 public class Brujula extends AppCompatActivity implements SensorEventListener {
 
     // Views donde se cargarán las cadenas de texto y la brújula
@@ -48,17 +67,21 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Inicializamos y enlazamos el view con el del xml
         setContentView(R.layout.activity_brujula);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        magnetic_field_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        accelerometer_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
         grados = (TextView) findViewById(R.id.grados);
         vientos = (ImageView) findViewById(R.id.imgViewCompass);
         listened = (TextView) findViewById(R.id.listened);
         message = (TextView) findViewById(R.id.message);
 
+        // Inicilizamos los sensores y el sensorManager.
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        magnetic_field_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometer_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Obtenemos el punto cardinal y la tolerancia que se reciben de la clase BrujulaVoz.java
+        // a través del Bundle.
         Intent intent = getIntent();
         Bundle b = intent.getBundleExtra(BrujulaVoz.EXTRA_BUNDLE);
 
@@ -74,6 +97,8 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
         else if (cardinal_point.equals("este"))
             objective = 90f;
 
+        // Mostramos por pantalla el objetivo al que le vamos a llevar y con qué tolerancia
+        // (lo que se supone que ha dicho el usuario).
         listened.setText( cardinal_point + " " + Float.toString(tolerance));
     }
 
@@ -81,6 +106,7 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
     protected void onResume(){
         super.onResume();
 
+        // Volvemos a recoger información de los sensores cuando la aplicación vuelve a ponerse en marcha.
         sensorManager.registerListener(this, accelerometer_sensor, SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(this, magnetic_field_sensor, SensorManager.SENSOR_DELAY_UI);
     }
@@ -88,12 +114,13 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
     @Override
     protected void onPause(){
         super.onPause();
-
+        // Paramos de recoger información de los sensores si la aplicación se pone en pausa.
         sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        // Cogemos los valores de los sensores
         if ( event.sensor.getType() == Sensor.TYPE_ACCELEROMETER )
             acel_data = event.values;
         else if ( event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD )
@@ -101,15 +128,20 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
 
         if( (acel_data != null) && (magnetic_data != null)){
             float rotation_matrix[] = new float[16];
+            // Calculamos la rotación del móvil y si dicho cálculo ha tenido éxito o no con la función
+            // getRotationMatrix().
             boolean success = SensorManager.getRotationMatrix(rotation_matrix, null, acel_data, magnetic_data);
 
             if(success){
                 float orientation[] = new float[3];
+                // Obtenemos la orientación.
                 SensorManager.getOrientation(rotation_matrix, orientation);
                 new_degree = orientation[0] * (180 / (float) Math.PI) ;
             }
         }
 
+        // Los grados van de 0 a 180, momento en el que van de 180 a 0 pero negativos.
+        // Según los grados obtenidos, mostramos si es el norte, sur...
         if (new_degree < 22.5 && new_degree > -22.5)
             grados.setText(Float.toString(new_degree) + "º" + " N");
         else if(new_degree > 22.5 && new_degree < 68)
@@ -127,6 +159,8 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
         else if(new_degree > -68 && new_degree < -22.5)
             grados.setText(Float.toString(new_degree) + "º" + " NO");
 
+        // Animamos la imagen de la brújula según los grados que ya tenía y
+        // hacia los que tiene que ir.
         RotateAnimation ra = new RotateAnimation(
                 current_degree,
                 new_degree,
@@ -142,6 +176,8 @@ public class Brujula extends AppCompatActivity implements SensorEventListener {
         vientos.startAnimation(ra);
         current_degree = -new_degree;
 
+        // Cuando se orienta en la dirección indicada que había dado por voz, avisamos de que
+        // ya se ha orientado como quería.
         if( Math.abs( new_degree - objective) < tolerance ||
                 ( objective == 180f && Math.abs( objective + new_degree ) < tolerance )    ){
 
--- appFotoVoz/java/BrujulaVoz.java
+++ appFotoVoz/java/BrujulaVoz.java
@@ -1,35 +1,40 @@
 package com.npi.appfotovoz;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Interpolator;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.BounceInterpolator;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+/*  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    See <http://www.gnu.org/licenses/> for a copy of the GNU General
+    Public License.
+
+    Autores: Jacinto Carrasco Castillo, Anabel Gómez Ríos.
+    Fecha de la última modificación: 10/02/2016.
+ */
+
+/* Esta clase se encarga de la primera pantalla visible de la aplicación, en la que aparece el botón
+   que permite el reconocimiento de voz al pulsarlo, busca una coincidencia en lo reconocido que tenga
+   el formato "punto_cardinal tolerancia", donde punto_cardinal será norte, sur, oeste o este y tolerancia
+   será un número y se encarga de pasárselo a la clase "Brújula.java", que será la que se encargue de mostrar
+   la brújula por pantalla.
+ */
 public class BrujulaVoz extends AppCompatActivity{
 
     public final static String EXTRA_BUNDLE = "com.npi.appfotovoz.BUNDLE";
@@ -38,53 +43,60 @@ public class BrujulaVoz extends AppCompatActivity{
     private TextView message_error;
 
     // Valores para el reconocimiento de voz
-
     private String language_model = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
 
     private static final String LOGTAG = "ASRBEGIN";
     private static int ASR_CODE = 123;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Inicializamos y enlazamos los Views con los del xml
         setContentView(R.layout.activity_brujula_voz);
 
         message_error = (TextView) findViewById(R.id.message_error);
 
+        // Llamamos al método para poner el botón para hablar al dispositivo
         setSpeekButton();
     }
 
-
+    /* Método que reconoce la voz cuando pulsamos el botón para hablar.
+     */
     private void listen(){
+        // Creamos el intent para reconocer lo que se dice
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
+        // Le decimos el idioma en el que se le está hablando y el número máximo de
+        // resultados que queremos que nos dé, fijado a 10.
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, language_model);
-
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
 
         startActivityForResult(intent, ASR_CODE);
     }
 
+    /* Método para poner el botón para poder hablar al dispositivo.
+     */
     private void setSpeekButton() {
         Button speak = (Button) findViewById(R.id.speech_button);
 
         speak.setOnClickListener(new View.OnClickListener() {
-                                     @Override
-                                     public void onClick(View v) {
-                                         if ("generic".equals(Build.BRAND.toLowerCase())) {
-                                             Toast toast = Toast.makeText(getApplicationContext(), "ASR no es soportado en dispositivos virtuales", Toast.LENGTH_SHORT);
-                                             toast.show();
-                                             Log.d(LOGTAG, "Intento de ASR en un dispositivo virtual");
-                                         } else {
-                                             listen();
-                                         }
-                                     }
-                                 }
-        );
+             @Override
+             public void onClick(View v) {
+                 if ("generic".equals(Build.BRAND.toLowerCase())) {
+                     Toast toast = Toast.makeText(getApplicationContext(), "ASR no es soportado en dispositivos virtuales", Toast.LENGTH_SHORT);
+                     toast.show();
+                     Log.d(LOGTAG, "Intento de ASR en un dispositivo virtual");
+                 } else {
+                     listen();
+                 }
+             }
+        });
     }
 
+    /* Método para, una vez tenemos el punto cardinal y el margen de error reconocidos
+       de lo que se ha dicho por voz, mandarle el punto cardinal y la tolerancia a la
+       clase "Brujula.java" a través de un Bundle.
+       Recibe como argumentos el punto cardinal en un String y un float con la tolerancia.
+    */
     private void sendMessage(String cardinal_point, float tolerance){
         Intent intent = new Intent(this, Brujula.class);
         Bundle b = new Bundle();
@@ -96,12 +108,18 @@ public class BrujulaVoz extends AppCompatActivity{
         startActivity(intent);
     }
 
+    /* Método para encontrar, del máximo de 10 resultados reconocidos que se han reconocido
+       por voz, aquel que tenga primero un punto cardinal y después un número, que será la
+       tolerancia.
+       Recibe como parámetros con array de strings con los mensajes reconocidos por voz.
+    */
     private void findBestMatch(ArrayList<String> strings){
-        String result = null;
         boolean found = false;
         float tolerance = -1;
         String cardinal_point = null;
 
+        // Buscamos entre los resultados hasta encontrar alguno con el formato
+        // "punto_cardinal tolerancia"
         for(int i = 0; i < strings.size() && !found ; i++){
             String string = strings.get(i);
 
@@ -111,6 +129,8 @@ public class BrujulaVoz extends AppCompatActivity{
                     cardinal_point = parts[0];
                     cardinal_point = cardinal_point.toLowerCase();
 
+                    // Si la segunda parte del mensaje reconocido no es un número no podremos
+                    // pasarlo a float. Controlamos esto con una excepción.
                     try{
                         tolerance = Float.parseFloat(parts[1]);
                     }
@@ -122,7 +142,6 @@ public class BrujulaVoz extends AppCompatActivity{
                                 cardinal_point.equals("sur") ||
                                 cardinal_point.equals("este") ||
                                 cardinal_point.equals("oeste")) && tolerance != -1) {
-                            result = string;
                             found = true;
                         }
                         else {
@@ -135,9 +154,12 @@ public class BrujulaVoz extends AppCompatActivity{
 
         }
 
+        // Si no encontramos ninguno con ese formato, indicamos cuál debe ser el formato.
         if( !found ){
             message_error.setText("La entrada debe ser de la forma: \"Punto Cardinal\" \"tolerancia\" ");
         }
+        // Si lo encontramos, llamamos al método que le pasa el punto cardinal y la tolerancia
+        // a la clase "Brujula.java".
         else{
             sendMessage(cardinal_point,tolerance);
         }
@@ -148,8 +170,10 @@ public class BrujulaVoz extends AppCompatActivity{
         if (requestCode == ASR_CODE){
             if (resultCode == RESULT_OK){
                 if (data != null) {
+                    // Recogemos los resultados que se han reconocido.
                     ArrayList<String> strings = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
+                    // Buscamos algún resultado que tenga el formato pedido.
                     findBestMatch(strings);
                 }
             }
